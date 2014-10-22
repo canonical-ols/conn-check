ae5bd1c0cdfe1f052302d93fe3bca882b5a893ba
Wes Mason <wesley.mason@canonical.com>
2014-10-22 16:52:53 -0400

    Completely remove wheels step from tarmac script

--- tarmac_tests.sh
+++ tarmac_tests.sh
@@ -4,11 +4,3 @@ set -e
 make clean
 make build
 make test
-
-if [ "$BUILD_CONN_CHECK_WHEELS" = "true" ]; then
-    # Update wheels branch with built packages
-    # DISABLED until we have a post-merge jenkins job
-    #make update-wheel-branch
-fi
-
-exit 0
