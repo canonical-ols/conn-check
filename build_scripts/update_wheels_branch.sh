05a7ec53a0fc0012ce3bbc866e62e825ef046323
Wes Mason <wesley.mason@canonical.com>
2014-10-23 16:11:47 -0400

    Consolidate revno check and wheels scripts

--- build_scripts/update_wheels_branch.sh
+++ build_scripts/update_wheels_branch.sh
@@ -16,10 +16,25 @@ if [[ -z "$WHEELS_BRANCH" ]]; then
 	exit 2
 fi
 
+if [[ -z "$WHEELS_BRANCH_DIR" ]]; then
+	>&2 echo "Please set the WHEELS_BRANCH_DIR variable first"
+	exit 2
+fi
+
+cd $(dirname $0)
+REVNO=$(bzr revno)
+
 cd $WHEELS_BRANCH_DIR
-bzr add *.whl
-bzr commit -m "Updating wheels from ${CONN_CHECK_REVNO}"
-bzr tag -d $WHEELS_BRANCH_DIR --force conn-check-r$CONN_CHECK_REVNO
-bzr push -d $WHEELS_BRANCH_DIR $WHEELS_BRANCH
+TAGS=$(bzr tags)
+
+if [[ "$TAGS" == *"conn-check-r$REVNO"* ]]; then
+	>&2 echo "revno already built and tagged, skipping"
+else
+	make test-wheels WHEELSDIR=$WHEELS_BRANCH_DIR
+	bzr add *.whl
+	bzr commit -m "Updating wheels from ${CONN_CHECK_REVNO}"
+	bzr tag -d $WHEELS_BRANCH_DIR --force conn-check-r$CONN_CHECK_REVNO
+	bzr push -d $WHEELS_BRANCH_DIR $WHEELS_BRANCH
+fi
 
 exit 0
