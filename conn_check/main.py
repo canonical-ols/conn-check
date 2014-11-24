e09087212cd8ee928caa8f80f7e6b07252e0cdb1
Wes Mason <wesley.mason@canonical.com>
2014-11-24 22:22:57 +0000

    Add comment to name parsing

--- conn_check/main.py
+++ conn_check/main.py
@@ -99,10 +99,15 @@ class OrderedOutput(object):
 
         name, message = data.split(' ', 1)
 
+        # Standard check name format is {type}:{host}:{port}
         name_parts = name.split(':', 3)
         try:
             name_parts[3] = ''
         except IndexError:
+            # This probably means a different name format,
+            # perhaps {type}:{host} with no port, but it's more trouble than
+            # it's worth trying to accomodate for these so we'll just skip
+            # grouping this check output correctly
             pass
         name = ':'.join(name_parts)
 
