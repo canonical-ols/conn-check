41f49d1ff4623ca528c2c8cc8e66500d467df654
Wes Mason <wesley.mason@canonical.com>
2015-06-19 16:16:27 +0100

    Don't trigger rtd on tarmac_tests, we need post-merge

--- tarmac_tests.sh
+++ tarmac_tests.sh
@@ -2,6 +2,4 @@
 set -e
 
 # Build and test conn-check
-make clean build test
-# Build sphinx docs and send webhook to readthedocs to update conn-check.rtfd.org
-make docs update-rtd
+make clean build test docs
